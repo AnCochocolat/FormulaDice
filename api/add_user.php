a912222f37f4e792cf9524645fc17dca341040f2
Takumi Nisato <g017a1337@gmail.com>
2020-01-27 14:57:18 +0900

    common modified

--- api/add_user.php
+++ api/add_user.php
@@ -3,7 +3,7 @@ require_once('common.php');
 
 $name=$_GET['name'];
 
-operate_DB('insert into UserData(name) values(?)',[$name]);
+add_DB('insert into UserData(name) values(?)',[$name]);
 
 $param = [
     'name' => $name
