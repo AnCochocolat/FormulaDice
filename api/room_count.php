ef95636b9ef154c41e4b394107fa8eb8abf6f5e1
Takumi Nisato <g017a1337@gmail.com>
2020-02-09 23:16:05 +0900

    room_count playercount add

--- api/room_count.php
+++ api/room_count.php
@@ -13,8 +13,15 @@ $result=get_DB($count_sql);
 //部屋人数を変数に保持
 $count=$result['max_count'];
 
+//現在のプレイヤー番号を取得
+$sql="select player_number from RoomList where room_name=$room";
+
+//プレイヤー番号
+$player_number=get_DB($sql);
+
 $param=[
-    'count'=>$count
+    'count'=>$count,
+    'player_count'=>$player_number
 ];
 
 echo json_encode($param); 
