173d329fc7f8b33f36acb22dba0f0a48bf551a87
Takumi Nisato <g017a1337@gmail.com>
2020-02-10 09:32:09 +0900

    room_get modi

--- api/room_get.php
+++ api/room_get.php
@@ -17,20 +17,40 @@ $player_number=get_DB($sql);
 $player_data=array(array());
 
     //テーブルから値を取得する
-    $sql="select _c_mileage as is_mileage,
-    _distance as is_distance,
-    _e_mileage as is_emileage,
-    _attack_use_mode as is_attack_usecount,
-    _attack_mode_count as is_attackcount from $room where id=?";
+    $sql="select _c_mileage as is_mileage from $room where id=?";
 
     //sqlから取得
     $result_sql=get_DB($sql,[$num_count+1]);
 
     //テーブルから取得してきた値
     $result_c_mileage=(int)$result_sql['is_mileage'];
+
+    $sql="select _distance as is_distance from $room where id=?";
+
+    //sqlから取得
+    $result_sql=get_DB($sql,[$num_count+1]);
+
     $result_distance=(int)$result_sql['is_distance'];
+
+    $sql="select _e_mileage as is_emileage from $room where id=?";
+
+    //sqlから取得
+    $result_sql=get_DB($sql,[$num_count+1]);
+
     $result_e_mileage=(int)$result_sql['is_emileage'];
+
+    $sql="select _attack_use_mode as is_attack_usecount from $room where id=?";
+
+    //sqlから取得
+    $result_sql=get_DB($sql,[$num_count+1]);
+
     $result_attackuse_count=(int)$result_sql['is_attack_usecount'];
+
+    $sql="select _attack_mode_count as is_attackcount from $room where id=?";
+
+    //sqlから取得
+    $result_sql=get_DB($sql,[$num_count+1]);
+    
     $result_attackmode_count=(int)$result_sql['is_attackcount'];
 
 //パラメータを返す
